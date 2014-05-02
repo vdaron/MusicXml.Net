b0d2859e744ea437af3b04613f8599fb66e7a219
Steven Solomon <solomon.steven.m@gmail.com>
2014-05-02 16:17:34 -0400

    Fix phrasing
    credit to https://github.com/willjames for corrections

--- MusicXml/Domain/Measure.cs
+++ MusicXml/Domain/Measure.cs
@@ -21,9 +21,9 @@ namespace MusicXml.Domain
 		/*
 		 * This structure contains the notes in the order they will appear in the rendered staff from highest to lowest in pitch.
 		 * 
-		 * Each key is the absolute postion of the note in the measure. The absolute position is caculated by summing the durations of notes as they are
-		 * parsed. If a backup or forward tag is encountered the current position is moved and the next insertion checks to see if there is
-		 * already a note at that position and performs and ordered insert.
+		 * Each key is the absolute postion of the note in the measure. The absolute position is calculated by summing the durations of notes as they are
+		 * parsed. If a backup or forward tag is encountered, the current position is moved and the next insertion checks to see if there is
+		 * already a note at that position and performs an ordered insert.
 		 */
 		public Dictionary <int, List<Note>> UpperStaffNotesInOrderOfTime { get; internal set; }
 	}

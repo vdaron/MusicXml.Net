323c996b76f914d5c21d0bab132aaa395c992499
Vincent DARON <vda@depfac.com>
2014-05-10 22:03:58 +0200

    Fix copyright and version number

--- MusicXml/Properties/AssemblyInfo.cs
+++ MusicXml/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MusicXml")]
-[assembly: AssemblyCopyright("Copyright © Vincent DARON 2008")]
+[assembly: AssemblyCopyright("Copyright © Vincent DARON - Steven Solomon 2008-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]

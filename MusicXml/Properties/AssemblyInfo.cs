8aadc3c92d84f0382b82216f93efeb6a20de3435
Vincent DARON <vda@depfac.com>
2014-05-10 22:05:51 +0200

    Add assembly description

--- MusicXml/Properties/AssemblyInfo.cs
+++ MusicXml/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MusicXml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MusicXML file parser for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MusicXml")]
